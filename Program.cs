989cd725d65fb9bb64ff3052a49dd186f9c1897b
sagret10 <sagret10@hotmail.com>
2018-09-23 16:00:50 -0300

    InANutshell_1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InANutshell_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
